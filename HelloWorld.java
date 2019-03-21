638cf3c8fba90a2ddebfb5ebb8058e197b0b1b88
ysyapr91 <ysyapr91@gmail.com>
2019-03-21 09:24:10 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package com.kubetest;
+public class HelloWorld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello gist @ GitHub!");   
+      }
+}  
